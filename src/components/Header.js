00e7ba28274e3d3af03cd5d72bdf71eb372c5857
j21w91 <j21w91@gmail.com>
2021-05-11 20:12:33 +0100

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>JW Detail & Valet</h1>
+        <p>
+          Attention to detail is what we do.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('services')
+            }}
+          >
+            Services
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('gallery')
+            }}
+          >
+            Gallery
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
